923de3b00ac93be8418ada12700955f9b6671f95
Yago Dias <yago.dias1409@gmail.com>
2023-03-23 00:14:45 -0300

    feat: Adding test controllers

---
+++ equipments-control-backend/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace equipments_control_backend.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
