d4e7836c1d0c256d8bf90e0fb9890bb155dd007e
Yago Dias <yago.dias1409@gmail.com>
2023-04-21 16:21:09 -0300

    feat: Equipment controller

---
+++ equipments-control-backend/Controllers/EquipmentsController.cs
@@ -0,0 +1,37 @@
+﻿using equipmentes_control.Persistence.Configs;
+using equipments_control.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace equipments_control_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentsController : ControllerBase
+    {
+        private readonly EquipmentContext equipmentContext;
+
+        public EquipmentsController(EquipmentContext equipmentContext)
+        {
+            this.equipmentContext = equipmentContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var equipment = await equipmentContext.Set<Equipment>().Include(a => a.Brand).Include(a => a.Department).SingleAsync(i => i.Id == id);
+
+            return Ok(equipment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Equipment equipment)
+        {
+            await equipmentContext.Set<Equipment>().AddAsync(equipment);
+            await equipmentContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
